db2b89d7d2533e48884de7f46b5a58848022e148
Kaarroolinaa <kaarroolinaa@gmail.com>
2025-02-27 15:08:26 +0200

    Added MainActivity

--- app/src/main/java/com/example/android_3/MainActivity.kt
+++ app/src/main/java/com/example/android_3/MainActivity.kt
@@ -1,47 +1,36 @@
 package com.example.android_3
 
+import android.content.Intent
 import android.os.Bundle
-import androidx.activity.ComponentActivity
-import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import com.example.android_3.ui.theme.Android_3Theme
+import androidx.appcompat.app.AppCompatActivity
 
-class MainActivity : ComponentActivity() {
+class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContent {
-            Android_3Theme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
-            }
-        }
+        setContentView(R.layout.activity_main)
+
+        supportFragmentManager.beginTransaction()
+            .replace(R.id.fragmentContainer, InputFragment())
+            .commit()
     }
-}
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
+    fun showResultFragment(selectedBook: String, selectedYear: String) {
+        val resultFragment = ResultFragment.newInstance(selectedBook, selectedYear)
 
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    Android_3Theme {
-        Greeting("Android")
+        supportFragmentManager.beginTransaction()
+            .replace(R.id.fragmentContainer, resultFragment)
+            .addToBackStack(null)
+            .commit()
+    }
+
+    fun showInputFragment() {
+        supportFragmentManager.beginTransaction()
+            .replace(R.id.fragmentContainer, InputFragment())
+            .commit()
+    }
+
+    fun openStorageActivity() {
+        val intent = Intent(this, StorageActivity::class.java)
+        startActivity(intent)
     }
 }
